c85343172b97329ceadc68ff30d4a6ab51ad6e1b
eeere <eeere@192.168.1.19>
2019-03-11 20:43:21 +0100

    Third Commit

--- TelegramBot/src/TelegramBot/Bot.java
+++ TelegramBot/src/TelegramBot/Bot.java
@@ -3,7 +3,29 @@ package TelegramBot;
 public class Bot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		import org.telegram.telegrambots.api.methods.send.SendMessage;
+		import org.telegram.telegrambots.api.objects.Update;
+		import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+		import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+		public class MyAmazingBot extends TelegramLongPollingBot {
+		    @Override
+		    public void onUpdateReceived(Update update) {
+		        // TODO
+		    }
+
+		    @Override
+		    public String getBotUsername() {
+		        // TODO
+		        return null;
+		    }
+
+		    @Override
+		    public String getBotToken() {
+		        // TODO
+		        return null;
+		    }
+		}
 
 	}
 
